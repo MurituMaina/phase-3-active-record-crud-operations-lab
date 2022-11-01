8ffb19af8daccc7260dd4cd71a511b4a48abbbaf
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-01 21:51:24 +0300

    commit tree f8e2ce0e823cc7e7f9fcc56f16d5b59507ebbbd0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,52 @@
 class Movie < ActiveRecord::Base
+  #   def self.find_all_movies_by_year(year)
+  #     Movie.where(year: year)
+  #   end
 
+  def self.create_with_title(title)
+    Movie.create(title: title)
+  end
+
+  def self.first_movie
+    Movie.first
+  end
+
+  def self.last_movie
+    Movie.last
+  end
+
+  def self.movie_count
+    Movie.count
+  end
+
+  def self.find_movie_with_id(id)
+    Movie.find(id)
+  end
+
+  def self.find_movie_with_attributes(attr)
+    Movie.find_by(attr)
+  end
+  def self.find_movies_after_2002
+    Movie.where("release_date > 2002")
+  end
+  # t.string "title"
+  # t.integer "release_date"
+  # t.string "director"
+  # t.string "lead"
+  # t.boolean "in_theaters"
+  def update_with_attributes(attributes)
+    self.update(attributes)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    Movie.delete(id)
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+end
 end
